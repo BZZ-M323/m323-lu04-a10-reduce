342cbfa8d73307fb99f1111d09e4b18c168ce912
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-15 11:52:35 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-def calculate():
+from functools import reduce
+
+# Der Algorithmus von Euklid ist etwas zu lange um ihn nur als Lambda-Funktion zu schreiben.
+# Deshalb schreiben wir eine Funktion, die den Algorithmus implementiert.
+def gcd(a, b):
     """
-    calculates the total
-    :return: None
+    Berechnet den größten gemeinsamen Teiler von a und b.
+    Args:
+    - a (int): Eine Zahl.
+    - b (int): Eine Zahl.
+    Returns:
+    - int: Der größte gemeinsame Teiler von a und b.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    pass
+
+
+def gcd(numbers):
+    """
+    Berechnet den größten gemeinsamen Teiler einer Liste von Zahlen.
+    Benutzt dazu die Funktion gcd(a, b) mit reduce().
+    Args:
+    - numbers (list): Eine Liste von Zahlen.
+    Returns:
+    - int: Der größte gemeinsame Teiler der Liste.
+    """
+    return 0
 
 
 if __name__ == '__main__':
-    calculate()
+    numbers = [12, 15, 21]
+    result = gcd(numbers)
+    print(result)  # Sollte 3 ausgeben
